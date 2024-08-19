8d650ecffee5aecfbe5ed1e3cefc6b509842af13
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-08-19 15:45:15 -0300

    oq eu to fazendo

--- COS242/permuta/target/debug/deps/permuta-09828b6cd68928e4.d
+++
@@ -1,5 +0,0 @@
-/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/permuta-09828b6cd68928e4: src/main.rs
-
-/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/permuta-09828b6cd68928e4.d: src/main.rs
-
-src/main.rs:
--- COS242/permuta/target/debug/libpermuta.d
+++
@@ -1,1 +0,0 @@
-/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/libpermuta.rlib: /home/kaway/Documentos/Projetos/college/COS242/permuta/src/lib.rs
