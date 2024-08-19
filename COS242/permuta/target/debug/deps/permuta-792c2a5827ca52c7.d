9454631c3968ecc6e911533a76d647772c3e1bc6
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-08-19 15:48:25 -0300

    fuck

---
+++ COS242/permuta/target/debug/deps/permuta-792c2a5827ca52c7.d
@@ -0,0 +1,7 @@
+/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/libpermuta-792c2a5827ca52c7.rmeta: src/lib.rs
+
+/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/libpermuta-792c2a5827ca52c7.rlib: src/lib.rs
+
+/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/permuta-792c2a5827ca52c7.d: src/lib.rs
+
+src/lib.rs:
