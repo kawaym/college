64dd38ad2b2552993e63750ade9b240d1a0492b7
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-08-19 15:32:06 -0300

    adds main function

---
+++ COS242/permuta/target/debug/deps/permuta-ad71ea64085e3160.d
@@ -0,0 +1,5 @@
+/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/libpermuta-ad71ea64085e3160.rmeta: src/main.rs
+
+/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/permuta-ad71ea64085e3160.d: src/main.rs
+
+src/main.rs:
