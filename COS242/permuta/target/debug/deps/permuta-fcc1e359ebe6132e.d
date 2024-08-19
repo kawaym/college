063294203a0c0ff96867c2a5a684e020692a82d4
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-08-19 15:42:44 -0300

    fixes shit

--- COS242/permuta/target/debug/deps/permuta-fcc1e359ebe6132e.d
+++
@@ -1,5 +0,0 @@
-/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/libpermuta-fcc1e359ebe6132e.rmeta: src/main.rs
-
-/home/kaway/Documentos/Projetos/college/COS242/permuta/target/debug/deps/permuta-fcc1e359ebe6132e.d: src/main.rs
-
-src/main.rs:
