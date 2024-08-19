cadf05a7e54edefc28c87818cc1a56c568408303
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-08-19 15:54:36 -0300

    fuckfucfujcjc

--- COS242/permuta/src/main.rs
+++ COS242/permuta/src/main.rs
@@ -5,11 +5,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let number = &args[1];
     let number = number.parse::<i32>().unwrap();
-    let mut list = (1..number + 1)
+    let mut number_list = (1..number + 1)
         .collect::<Vec<i32>>()
         .into_iter()
         .map(|x| Some(x))
         .collect::<Vec<Option<i32>>>();
 
-    permute(0, (number - 1).try_into().unwrap(), &mut list)
+    permute(0, (number - 1).try_into().unwrap(), &mut number_list)
 }
