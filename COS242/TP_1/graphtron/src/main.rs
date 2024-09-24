ea3fb2c6a981f1199a9ba79f2eb4aef232113328
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-09-24 20:02:07 -0300

    adds function to measure distance of all graphs

--- COS242/TP_1/graphtron/src/main.rs
+++ COS242/TP_1/graphtron/src/main.rs
@@ -1,13 +1,40 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+
 use graphtron::parser;
 
 fn main() {
-    let mut graph = parser::read_graph("./data/grafo_1.txt");
+    // let mut graph = parser::read_graph("./data/grafo_1.txt");
     // graph.display("Lista".to_string());
     // let dfs = graph.dfs("1");
     // graph.display_dfs("1");
     // println!("------------------------------");
     // graph.display_bfs("1");
-    graph.display_distance("4", "1");
-    graph.calculate_diameter();
+    // graph.display_distance("4", "1");
+    // graph.calculate_diameter();
+
+    let _ = measure_distance("./data");
     // println!("DFS: {:?}", dfs);
+}
+
+fn measure_distance(dir: &str) -> io::Result<()> {
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_file() {
+            if let Some(path_str) = path.to_str() {
+                let mut graph = parser::read_graph(path_str);
+                let diameter = graph.calculate_diameter();
+
+                println!(
+                    "O diâmetro para o grafo do arquivo {} é de: {}",
+                    path_str, diameter
+                );
+            }
+        }
+    }
+
+    Ok(())
 }
