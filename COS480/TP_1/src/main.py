efaec9f37a2df30b643665348b63c9607eb21069
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-09-29 14:36:43 -0300

    changes header to read from records file

--- COS480/TP_1/src/main.py
+++ COS480/TP_1/src/main.py
@@ -1,7 +1,12 @@
 from disk import start_db, load_db_data
 from sql import delete_many_by_field
 from fetch import dataset
+from constants import HEADER
+from fixed_heap_sql import insert_one
 
 start_db()
-load_db_data("digimons")
+HEADER.read_header_from_json()
+# HEADER.write_header_to_json()
+# insert_one(dataset.iloc[0])
+# load_db_data("digimons")
 # delete_many_by_field("Electric", "Attribute")
